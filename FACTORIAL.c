efc6de91d2d0dbc0f437cf0bcea778b1388761e7
Prawin Kumar J S <155090799+Prawinkumarjs@users.noreply.github.com>
2025-05-06 22:18:43 +0530

    Update FACTORIAL.c

--- FACTORIAL.c
+++ FACTORIAL.c
@@ -31,3 +31,25 @@ int main()
    printf("%d",fact);
    return 0;
 }
+
+// factorial using do while loop
+
+#include <stdio.h>
+
+int main()
+{
+    int i=1, fact=1, num;
+    printf("Enter the num: ");
+    scanf("%d", &num);
+    
+    do{
+        fact = fact*i;
+        i++;
+    }while(i<=num);
+    
+    printf("%d\n",fact);
+    
+    printf("%d",i);
+
+    return 0;
+}
