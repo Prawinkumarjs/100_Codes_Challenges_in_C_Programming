1ef5ae2d639303ed9384b3d40242d10d2a60cf3c
Prawin Kumar J S <155090799+Prawinkumarjs@users.noreply.github.com>
2025-07-05 23:08:08 +0530

    Create HCF Of Two Numbers.c

---
+++ HCF Of Two Numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, hcf = 1;
+    
+    printf("Enter num1: ");
+    scanf("%d", &num1);
+    
+    printf("Enter num2: ");
+    scanf("%d", &num2);
+    
+    for(int i = 1; i <= num1 || i <= num2; i++){
+        
+        if(num1 % i == 0 && num2 % i == 0 ){
+            hcf = i;
+        }
+    }
+    
+    printf("The HCF is %d \n", hcf);
+    
+
+    return 0;
+}
