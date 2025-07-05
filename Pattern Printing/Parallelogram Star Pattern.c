998100dbcea8574dbfd38364cd40062d92afed4f
Prawin Kumar J S <155090799+Prawinkumarjs@users.noreply.github.com>
2025-07-05 22:15:53 +0530

    Create Parallelogram Star Pattern.c

---
+++ Pattern Printing/Parallelogram Star Pattern.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,n,m;
+    
+    printf("Enter n/ row: ");
+    scanf("%d",&n);
+   
+    printf("Enter n/ column: ");
+    scanf("%d",&m);
+    
+    
+    
+    
+    for(i = 0; i < n; i++){
+        
+        for(j = 0; j <i; j++){
+            
+            printf(" ");
+            
+        }
+        
+        for(j = 0; j < m; j++)
+            {
+                printf("*");
+            }
+        
+        printf("\n");
+    }
+
+    return 0;
+}
